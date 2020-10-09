1d7bc11de7974e8bc2a001c359c2b151eea86091
Beatriz <beatrizmcout@gmail.com.br>
2020-10-09 11:02:20 -0300

    adicionado menu principal e seleção de opções

---
+++ src/interfaces/Menu.java
@@ -0,0 +1,17 @@
+package interfaces;
+
+public class Menu {
+	public static void mostrarMenu() {
+		System.out.println("\n*********************");
+		System.out.println("AGENDA DE CLIENTES");
+		System.out.println("*********************");
+		System.out.println("1. Cadastrar");
+		System.out.println("2. Editar");
+		System.out.println("3. Excluir");
+		System.out.println("4. Listar");
+		System.out.println("5. Gerar relatórios");
+		System.out.println("6. Sair");
+		System.out.println("*********************\n");
+	}
+
+}
